eff9c40d38401af4d3367ea799fd64a94f742f27
Dan Carlier <dan.carlier@gmail.com>
2023-02-24 16:18:04 +1100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="app">
+    <p>{{ jobs[0].location }}</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import type Job from './types/Job'
+
+const jobs = ref<Job[]>([
+  { title: 'farm worker', location: 'lon lon ranch', salary: 30000, id: '1'},
+  { title: 'quarryman', location: 'death mountain', salary: 40000, id: '2'},
+  { title: 'flute player', location: 'the lost woods', salary: 35000, id: '3'},
+  { title: 'fisherman', location: 'lake hylia', salary: 21000, id: '4'},
+  { title: 'prison guard', location: 'gerudo valley', salary: 32000, id: '5'},
+])
+</script>
+
+<style scoped>
+</style>
